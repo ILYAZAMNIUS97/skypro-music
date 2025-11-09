58f0ae57e7a7dcaf3df37a80821570a84d8c04eb
ILYAZAMNIUS97 <zamnius.ilya@yandex.ru>
2025-11-09 11:22:18 +0300

    Доработка дизайна

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,10 +5,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import classNames from 'classnames';
 import { Fade as Hamburger } from 'hamburger-react';
+import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { logoutUser } from '@/store/authSlice';
 import styles from './Navigation.module.css';
 
 export const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+
+  const handleLogout = async () => {
+    await dispatch(logoutUser());
+    setIsMenuOpen(false);
+  };
 
   return (
     <nav className={styles.nav}>
@@ -49,9 +58,19 @@ export const Navigation = () => {
             </Link>
           </li>
           <li className={styles.menuItem}>
-            <Link href="/auth/signin" className={styles.menuLink}>
-              Войти
-            </Link>
+            {isAuthenticated ? (
+              <button
+                onClick={handleLogout}
+                className={styles.menuLink}
+                type="button"
+              >
+                Выйти
+              </button>
+            ) : (
+              <Link href="/auth/signin" className={styles.menuLink}>
+                Войти
+              </Link>
+            )}
           </li>
         </ul>
       </div>
